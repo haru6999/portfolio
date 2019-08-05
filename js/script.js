9693642cf5e77b196148f2dc73fe7186eec97344
Haruka Goto <harubj6999@gmail.com>
2019-08-06 01:28:12 +0900

    メニューバー綺麗に回った

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 jQuery(window).on('load', function() {
-    $(function() {
-        $(".menu-trigger").click(function() {
-            $(this).toggleClass("active");
-            $(".yellow").toggleClass("active");
-            $(".purple").toggleClass("active");
-            $("header li").toggleClass("active");
-            if($('.li').hasClass('active')){
-                setTimeout(function(){
-                    $('.li').css('opacity','1');
-                },400);
-            }else{
-                $('.li').css('opacity','0');
-            }
-        });
-        
+    
+});
+
+
+$(function() {
+    $(".menu-trigger").click(function() {
+        $(this).toggleClass("active");
+        $(".yellow").toggleClass("active");
+        $(".purple").toggleClass("active");
+        $("header li").toggleClass("active");
+        if($('.li').hasClass('active')){
+            setTimeout(function(){
+                $('.li').css('opacity','1');
+            },400);
+        }else{
+            $('.li').css('opacity','0');
+        }
     });
+    
 });
