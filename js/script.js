ef23b4569759a481c4ab061c81d230beeecf1553
Haruka Goto <harubj6999@gmail.com>
2019-08-14 21:11:57 +0900

    提出

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
 jQuery(window).on('load', function() {
-    
-});
-
-
-$(function() {
-    $(".menu-trigger").click(function() {
-        $(this).toggleClass("active");
-        $(".yellow").toggleClass("active");
-        $(".purple").toggleClass("active");
-        $("header li").toggleClass("active");
-        if($('.li').hasClass('active')){
-            setTimeout(function(){
-                $('.li').css('opacity','1');
-            },400);
-        }else{
-            $('.li').css('opacity','0');
-        }
-    });
-    
+    $(function() {
+        // headerのmenubar
+        $(".menu-trigger").click(function() {
+            $(this).toggleClass("active");
+            $(".yellow").toggleClass("active");
+            $(".purple").toggleClass("active");
+            $("header li").toggleClass("active");
+            if($('.li').hasClass('active')){
+                setTimeout(function(){
+                    $('.li').css('opacity','1');
+                },400);
+            }else{
+                $('.li').css('opacity','0');
+            }
+        });
+        
+        // 画面スクロール
+        var offsetY = -10;
+        var time = 500;
+        $('#profile').click(function() {
+            console.log("yes");
+            var target = $(this.hash);
+            if (!target.length) return ;
+            var targetY = target.offset().top+offsetY;
+            $('html,body').animate({scrollTop: targetY}, time, 'swing');
+            window.history.pushState(null, null, this.hash);
+            return false;
+        });
+    }); 
 });
